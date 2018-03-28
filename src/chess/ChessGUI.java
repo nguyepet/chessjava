50a3b88e0b0bb72915966f0bcd09d0975965071f
nguyepet <peternwin@gmail.com>
2018-03-28 17:58:33 -0400

    start gui

---
+++ src/chess/ChessGUI.java
@@ -0,0 +1,23 @@
+package chess;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ChessGUI {
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Chess Game");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		ChessPanel panel = new ChessPanel();
+		frame.getContentPane().add(panel);
+		
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+}
--- src/chess/ChessPanel.java
+++ src/chess/ChessPanel.java
@@ -1,5 +1,55 @@
 package chess;
 
-public class ChessPanel{
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
+public class ChessPanel extends JPanel{
+		
+		/* the display buttons for board */
+		private JButton[][] board;
+		
+		/* the chess model to run game*/
+		private ChessModel model;
+		
+		/* The image icons  */
+		private ImageIcon iconBlank;
+		private ImageIcon iconBKing;
+		private ImageIcon iconBQueen;
+		private ImageIcon iconB;
+		
+		/* listener for game action events*/
+		private ButtonListener listener = new ButtonListener();
+		
+		
+		/********************************************************************
+		 * Constructor for chess panel
+		 *******************************************************************/
+		public ChessPanel() {
+			
+			
+			setLayout(new GridLayout(8, 8));
+			setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+			
+			
+		}
+		
+		/********************************************************************
+		 *  Method that updates the board
+		 ********************************************************************/
+		private void displayBoard() {
+			
+		}
+		
+		/********************************************************************
+		 * Action listener for buttons 
+		 ********************************************************************/
+		private class ButtonListener implements ActionListener{
+			
+			public void actionPerformed(ActionEvent event) {
+				
+			}
+		}
+		
+		
 }
